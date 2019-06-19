8443988a33690337e20558c3f24070dab7e4cb6f
yiwen <iceax@gmail.com>
2019-06-19 13:48:51 +0200

    my-first-blog

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+from django.shortcuts import redirect
 from .models import Post
+
 # Create your views here.
 def post_list(request):
     Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -8,3 +10,4 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404 (Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+    
